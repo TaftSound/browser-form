72051a6b3f7f3f027ea08be766c6e5571b6d1063
TaftSound <mattmurphytattoos@gmail.com>
2022-09-19 10:50:54 -0700

    Style form

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,12 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: {
+    index: './src/index.js',
+  },
   devtool: 'inline-source-map',
   devServer: {
     static: './dist',
@@ -10,7 +14,18 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+    new CopyPlugin ({
+      patterns: [
+        { from: "src/images/", to: "images/"},
+      ],
+    }),
+  ],
   module: {
     rules: [
       {
